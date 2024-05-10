e2ac93b03330e80e916c6e8055f24ffd42f80db7
AKAxedx <1365957941@qq.com>
2024-05-11 00:33:52 +0800

    init

---
+++ src/main/java/anda/selectlesson/SelectLessonApplication.java
@@ -0,0 +1,13 @@
+package anda.selectlesson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SelectLessonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SelectLessonApplication.class, args);
+    }
+
+}
---
+++ src/test/java/anda/selectlesson/SelectLessonApplicationTests.java
@@ -0,0 +1,13 @@
+package anda.selectlesson;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SelectLessonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
